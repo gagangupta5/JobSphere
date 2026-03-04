5a27e4f623f32e9cdcbffc1f27b618c3c667e80d
Gagan Gupta <2k23.cscys2310911@gmail.com>
2026-03-04 21:18:16 +0530

    Updated Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/components_lite/Navbar";
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/components_lite/Home";
+import PrivacyPolicy from "./components/components_lite/PrivacyPolicy.jsx";
+import TermsofService from "./components/components_lite/TermsofService.jsx";
+import Jobs from "./components/components_lite/Jobs.jsx";
+import Browse from "./components/components_lite/Browse.jsx";
+import Profile from "./components/components_lite/Profile.jsx";
+import Description from "./components/components_lite/Description.jsx";
+import Companies from "./components/admincomponent/Companies.jsx";
+import CompanyCreate from "./components/admincomponent/CompanyCreate";
+import CompanySetup from "./components/admincomponent/CompanySetup";
+import AdminJobs from "./components/admincomponent/AdminJobs.jsx";
+import PostJob from "./components/admincomponent/PostJob";
+import Applicants from "./components/admincomponent/Applicants";
+import ProtectedRoute from "./components/admincomponent/ProtectedRoute";
+import Creator from "./components/creator/Creator.jsx";
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/description/:id",
+    element: <Description />,
+  },
+  {
+    path: "/Profile",
+    element: <Profile />,
+  },
+  {
+    path: "/PrivacyPolicy",
+    element: <PrivacyPolicy />,
+  },
+  {
+    path: "/TermsofService",
+    element: <TermsofService />,
+  },
+  {
+    path: "/Jobs",
+    element: <Jobs />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    path: "/Browse",
+    element: <Browse />,
+  },
+  {
+    path:"/Creator",
+    element: <Creator/>
+  },
+
+  // /admin
+  {
+    path: "/admin/companies",
+    element: (
+      <ProtectedRoute>
+        <Companies />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/companies/create",
+    element: (
+      <ProtectedRoute>
+        <CompanyCreate />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/companies/:id",
+    element: (
+      <ProtectedRoute>
+        <CompanySetup />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs",
+    element: (
+      <ProtectedRoute>
+        {" "}
+        <AdminJobs />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs/create",
+    element: (
+      <ProtectedRoute>
+        {" "}
+        <PostJob />{" "}
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/admin/jobs/:id/applicants",
+    element: (
+      <ProtectedRoute>
+        <Applicants />
+      </ProtectedRoute>
+    ),
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <RouterProvider router={appRouter}></RouterProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
